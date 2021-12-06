b5cb017a0d31eb480e3f12b6e9cd97489ea42a68
grossjor <jorik.gross@stud.hawk.de>
2021-12-06 12:50:39 +0100

    neuster build in ornder neuster build

---
+++ Kaffeeautomat/Neuster Build/Kaffeautomat.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <windows.h>
+
+int main () {
+system("cls");
+std::string n = "astring";
+std::cout << "Herzlich Willkommen beim Kaffee-Automaten" << '\n';
+std::cout << "Bitte w"<<(char)132<<"hlen sie aus:" << '\n';
+std::cout << "(k) Kaffee" << '\n';
+std::cout << "(e) Espresso" << '\n';
+std::cout << "(s) Service Mode" << '\n';
+std::cin >> n;
+
+
+if(n=="k"){
+  std::cout << "Sie haben sich f"<<(char)129<<"r Auswahl e Entschieden" << '\n';
+  std::cout << "Ihr Getr"<<(char)132<<"nk wird zubereitet" << '\n';
+    sleep (3);
+    std::cout << "Kaffee ist Fertig" << '\n';
+      sleep (3);
+      return main();
+
+
+}
+if(n=="e"){
+  std::cout << "Sie haben sich f"<<(char)129<<"r Auswahl e Entschieden" << '\n';
+  std::cout << "Ihr Getr"<<(char)132<<"nk wird zubereitet" << '\n';
+    sleep (3);
+    std::cout << "Espresso ist Fertig" << '\n';
+      sleep (3);
+      return main();
+
+}
+if(n=="s"){
+  std::cout << "Sie haben sich f"<<(char)129<<"r Auswahl e Entschieden" << '\n';
+  std::cout << "Der Servicemode ist zuzeit nicht verf"<<(char)207<<"gbar" << '\n';
+    sleep (3);
+    std::cout << "Zurück zum Men"<<(char)207<<"" << '\n';
+      sleep (3);
+      return main();
+
+}
+return main();
+}
