3818f745b34f546d2d7a8059120e130d33859cb1
grossjor <jorik.gross@stud.hawk.de>
2021-12-13 12:19:12 +0100

    antwort 4.1.6

---
+++ Blatt8/typumwandlung.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <windows.h>
+int main() {
+  int l;
+  double laenge{ 100.05 };
+   char zeichen;
+   l = laenge;
+   zeichen = laenge;
+   std::cout << laenge << " " << l << " " << zeichen << endl;
+ string messlatte { "777.77" };
+   laenge = static_cast<double>(messlatte);
+  return 0;
+}
