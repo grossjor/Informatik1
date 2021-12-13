67485b685faa985a2abed09b14535e113a63a1f6
grossjor <jorik.gross@stud.hawk.de>
2021-12-13 12:31:34 +0100

    bugfixes und compilieren

--- Blatt8/typumwandlung.cpp
+++ Blatt8/typumwandlung.cpp
@@ -5,14 +5,20 @@
 #include <time.h>
 #include <unistd.h>
 #include <windows.h>
+
 int main() {
+  using std::cout;
+using std::endl;
+using std::string;
+
   int l;
+
   double laenge{ 100.05 };
    char zeichen;
    l = laenge;
    zeichen = laenge;
    std::cout << laenge << " " << l << " " << zeichen << endl;
  string messlatte { "777.77" };
-   laenge = static_cast<double>(messlatte);
+   //laenge = static_cast<double>(messlatte);
   return 0;
 }
