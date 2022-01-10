2dc819c5b2cea01d4032b5883c58cfd4ac5bc4bf
grossjor <jorik.gross@stud.hawk.de>
2022-01-10 18:17:41 +0100

    Final Build

--- Blatt9/Kaffeautomat.cpp
+++ Blatt9/Kaffeautomat.cpp
@@ -26,94 +26,106 @@ Rueckgeld = 0;
 
 
 
-  while (true) {
+while (true) {
 
 
-system("cls");
-std::string milk = "astring";
-std::string sugar ="astring";
+  system("cls");
+    std::string milk = "astring";
+    std::string sugar ="astring";
+    std::string n = "astring";
+    std::cout << "Herzlich Willkommen beim Kaffee-Automaten" << '\n';
+    std::cout << "Preis pro Tasse: 1 Euro" << '\n';
+    std::cout << "Milch oder Zucker: 10 Cent" << '\n';
+    std::cout << "-------------------------------------------------------" << '\n';
+    std::cout << "Bitte w"<<(char)132<<"hlen sie aus:" << '\n';
+    std::cout << "(k) Kaffee" << '\n';
+    std::cout << "(e) Espresso" << '\n';
+    std::cout << "(s) Service Mode" << '\n';
+    std::cin >> n;
 
-std::string n = "astring";
-std::cout << "Herzlich Willkommen beim Kaffee-Automaten" << '\n';
-std::cout << "Preis pro Tasse: 1 Euro" << '\n';
-std::cout << "Milch oder Zucker: 10 Cent" << '\n';
-std::cout << "-------------------------------------------------------" << '\n';
-std::cout << "Bitte w"<<(char)132<<"hlen sie aus:" << '\n';
-std::cout << "(k) Kaffee" << '\n';
-std::cout << "(e) Espresso" << '\n';
-std::cout << "(s) Service Mode" << '\n';
-std::cin >> n;
 
+    if(n=="k"){
+      std::cout << "Sie haben sich f"<<(char)129<<"r Auswahl (K)affee Entschieden" << '\n';
+      Preis = 1;
+      std::cout << "Wollen sie Milch? (j/n)" << '\n';
+      std::cin >> milk;
+        if(milk == "j" ){ Preis = Preis+0.1;}
+        if(milk == "n" ){}
+          std::cout << "Wollen sie Zucker? (j/n)" << '\n';
+          std::cin >> sugar;
+        if(sugar == "j" ){ Preis = Preis+0.1;}
+        if(sugar == "n" ){}
+          std::cout << "Bitte Zahlen sie :" <<Preis << '\n';
+          std::cin >> Geld;
+            if (Geld < Preis) {
+              std::cout << "Zu wenig Geld, Vorgang wird abgebrochen." << '\n';
+              sleep (2);
+                }
+            if (Geld >= Preis) {
+              std::cout << "Ihr Getr"<<(char)132<<"nk wird zubereitet" << '\n';
+              sleep (3);
+              std::cout << "Kaffee ist Fertig" << '\n';
+                  if (sugar == "j") {
+                    Zucker = Zucker - 5;
 
-if(n=="k"){
-  std::cout << "Sie haben sich f"<<(char)129<<"r Auswahl k Entschieden" << '\n';
-  Preis = 1;
-  std::cout << "Wollen sie Milch? (j/n)" << '\n';
-  std::cin >> milk;
-    if(milk == "j" ){ Preis = Preis+0.1;}
-      if(milk == "n" ){}
-      std::cout << "Wollen sie Zucker? (j/n)" << '\n';
-      std::cin >> sugar;
-      if(sugar == "j" ){ Preis = Preis+0.1;}
-      if(sugar == "n" ){}
-      std::cout << "Bitte Zahlen sie :" <<Preis << '\n';
-      std::cin >> Geld;
-        if (Geld < Preis) {
-          std::cout << "Zu wenig Geld, Vorgang wird abgebrochen." << '\n';
-          sleep (2);
-        }
-        if (Geld >= Preis) {
-          std::cout << "Ihr Getr"<<(char)132<<"nk wird zubereitet" << '\n';
-          sleep (3);
-      std::cout << "Kaffee ist Fertig" << '\n';
-      if (sugar == "j") {
-        Zucker = Zucker - 5
+                  }
+                  if (milk == "j") {
+                    Milch = Milch - 30;
+                  }
+                  Rueckgeld = Geld - Preis;
+                  Kaffeebohnen = Kaffeebohnen - 5;
+                  Wasser = Wasser - 125;
+          std::cout << "Bitte entnehmen sie " <<Rueckgeld <<" Wechselgeld" << '\n';
       }
-      if (milk == "j") {
-        Milch = Milch - 30
-      }
-      Rueckgeld = Geld - Preis;
-      std::cout << "Bitte entnehmen sie " <<Rueckgeld <<"Wechselgeld" << '\n';
+      sleep (2);
     }
-}
-if(n=="e"){
-  std::cout << "Sie haben sich f"<<(char)129<<"r Auswahl e Entschieden" << '\n';
-  Preis = 1;
-  std::cout << "Wollen sie Milch? (j/n)" << '\n';
-  std::cin >> milk;
-    if(milk == "j" ){ Preis = Preis+0.1;}
-      if(milk == "n" ){}
-      std::cout << "Wollen sie Zucker? (j/n)" << '\n';
-      std::cin >> sugar;
-      if(sugar == "j" ){ Preis = Preis+0.1;}
-      if(sugar == "n" ){}
-      std::cout << "Bitte Zahlen sie :" <<Preis << '\n';
-      std::cin >> Geld;
-        if (Geld < Preis) {
-          std::cout << "Zu wenig Geld, Vorgang wird abgebrochen." << '\n';
-          sleep (2);
-        }
-        if (Geld >= Preis) {
-          std::cout << "Ihr Getr"<<(char)132<<"nk wird zubereitet" << '\n';
-          sleep (3);
-      std::cout << "Kaffee ist Fertig" << '\n';
-      Rueckgeld = Geld - Preis;
-      std::cout << "Bitte entnehmen sie " <<Rueckgeld <<"Wechselgeld" << '\n';
-    }
-  }
+    if(n=="e"){
+      std::cout << "Sie haben sich f"<<(char)129<<"r Auswahl (E)spresso Entschieden" << '\n';
+      Preis = 1;
+      std::cout << "Wollen sie Milch? (j/n)" << '\n';
+      std::cin >> milk;
+        if(milk == "j" ){ Preis = Preis+0.1;}
+        if(milk == "n" ){}
+          std::cout << "Wollen sie Zucker? (j/n)" << '\n';
+          std::cin >> sugar;
+            if(sugar == "j" ){ Preis = Preis+0.1;}
+            if(sugar == "n" ){}
+              std::cout << "Bitte Zahlen sie :" <<Preis << '\n';
+              std::cin >> Geld;
+                if (Geld < Preis) {
+                  std::cout << "Zu wenig Geld, Vorgang wird abgebrochen." << '\n';
+                  sleep (2);
+                }
+                  if (Geld >= Preis) {
+                    std::cout << "Ihr Getr"<<(char)132<<"nk wird zubereitet" << '\n';
+                    sleep (3);
+                    std::cout << "Kaffee ist Fertig" << '\n';
+                  if (sugar == "j") {
+                    Zucker = Zucker - 5;
 
-if(n=="s"){
-  std::cout << "Sie haben sich f"<<(char)129<<"r Auswahl s Entschieden" << '\n';
-  std::cout << "Der Servicemode ist zuzeit nicht verf"<<(char)129<<"gbar" << '\n';
-    sleep (3);
-    std::cout << "Zur"<<(char)129<<"ck zum Men"<<(char)129<<"" << '\n';
-    std::cout << "Kaffebohnen"<<Kaffeebohnen << '\n';
-    std::cout << "Espressobohnen"<<Espressobohnen << '\n';
-    std::cout << "Wassser"<<Wasser << '\n';
-    std::cout << "Milch"<<Milch << '\n';
-    std::cout << "Zucker"<<Zucker << '\n';
+                  }
+                  if (milk == "j") {
+                    Milch = Milch - 30;
+                  }
+                  Rueckgeld = Geld - Preis;
+                  Wasser = Wasser - 125;
+                  Espressobohnen = Espressobohnen - 5;
+                  std::cout << "Bitte entnehmen sie " <<Rueckgeld <<" Wechselgeld" << '\n';
+                }
+              }
+
+
+    if(n=="s"){
+        std::cout << "Sie haben sich f"<<(char)129<<"r Auswahl s Entschieden" << '\n';
+        std::cout << "Kaffebohnen"<<Kaffeebohnen << '\n';
+        std::cout << "Espressobohnen"<<Espressobohnen << '\n';
+        std::cout << "Wassser"<<Wasser << '\n';
+        std::cout << "Milch"<<Milch << '\n';
+        std::cout << "Zucker"<<Zucker << '\n';
+        sleep (5);
+        std::cout << "Zur"<<(char)129<<"ck zum Men"<<(char)129<<"" << '\n';
 }
-sleep (2);
+  sleep (2);
 }
 
 }
